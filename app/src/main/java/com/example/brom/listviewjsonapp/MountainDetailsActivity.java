325e0b1ae826d1fed5d84347c5eade5887e67a0b
Peter Håkansson <a18petha@student.his.se>
2019-04-23 11:09:19 +0200

    load activity

---
+++ app/src/main/java/com/example/brom/listviewjsonapp/MountainDetailsActivity.java
@@ -0,0 +1,23 @@
+package com.example.brom.listviewjsonapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.brom.activitiesapp.R;
+
+public class MountainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mountain_details);
+        Intent intent = getIntent();
+
+        TextView test = (TextView) findViewById(R.id.world);
+        intent.getStringExtra("mountainNames");
+        test.setText("Load");
+
+    }
+}
